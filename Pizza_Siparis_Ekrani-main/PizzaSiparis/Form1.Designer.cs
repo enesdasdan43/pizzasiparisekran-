0adb7be1c46357e68aeccb23d1566406c4e066f8
enesdasdan43 <257017028@ogrenci.sinop.edu.tr>
2026-03-04 21:18:20 +0300

    Add files via upload

---
+++ Pizza_Siparis_Ekrani-main/PizzaSiparis/Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace PizzaSiparis
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblEbat = new System.Windows.Forms.Label();
+            this.cbEbat = new System.Windows.Forms.ComboBox();
+            this.lstTür = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdet = new System.Windows.Forms.TextBox();
+            this.txtSiparisToplam = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblEbat
+            // 
+            this.lblEbat.AutoSize = true;
+            this.lblEbat.BackColor = System.Drawing.Color.Transparent;
+            this.lblEbat.Location = new System.Drawing.Point(109, 81);
+            this.lblEbat.Name = "lblEbat";
+            this.lblEbat.Size = new System.Drawing.Size(38, 16);
+            this.lblEbat.TabIndex = 0;
+            this.lblEbat.Text = "Ebat:";
+            // 
+            // cbEbat
+            // 
+            this.cbEbat.FormattingEnabled = true;
+            this.cbEbat.Items.AddRange(new object[] {
+            "Double Small 100Tl",
+            "Small 120Tl",
+            "Medium 150TL",
+            "Large 210Tl",
+            "Double Large 250Tl"});
+            this.cbEbat.Location = new System.Drawing.Point(150, 78);
+            this.cbEbat.Name = "cbEbat";
+            this.cbEbat.Size = new System.Drawing.Size(164, 24);
+            this.cbEbat.TabIndex = 1;
+            // 
+            // lstTür
+            // 
+            this.lstTür.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lstTür.FormattingEnabled = true;
+            this.lstTür.ItemHeight = 16;
+            this.lstTür.Items.AddRange(new object[] {
+            "American Hot",
+            "Callypso",
+            "Turkish",
+            "Pizza İtaliano",
+            "Etravaganzza"});
+            this.lstTür.Location = new System.Drawing.Point(112, 108);
+            this.lstTür.Name = "lstTür";
+            this.lstTür.Size = new System.Drawing.Size(202, 100);
+            this.lstTür.TabIndex = 2;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Location = new System.Drawing.Point(113, 224);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(91, 20);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "İnce Kenar";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Location = new System.Drawing.Point(210, 224);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(95, 20);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Kalın Kenar";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.checkBox9);
+            this.groupBox1.Controls.Add(this.checkBox10);
+            this.groupBox1.Controls.Add(this.checkBox7);
+            this.groupBox1.Controls.Add(this.checkBox8);
+            this.groupBox1.Controls.Add(this.checkBox5);
+            this.groupBox1.Controls.Add(this.checkBox6);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(112, 250);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(232, 224);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Malzemeler";
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(134, 198);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(67, 20);
+            this.checkBox9.TabIndex = 9;
+            this.checkBox9.Text = "Peynir";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(1, 198);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(65, 20);
+            this.checkBox10.TabIndex = 8;
+            this.checkBox10.Text = "Zeytin";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(134, 159);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(90, 20);
+            this.checkBox7.TabIndex = 7;
+            this.checkBox7.Text = "Ton Balığı";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(1, 159);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(73, 20);
+            this.checkBox8.TabIndex = 6;
+            this.checkBox8.Text = "Ançuez";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(134, 118);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(70, 20);
+            this.checkBox5.TabIndex = 5;
+            this.checkBox5.Text = "Mantar";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(1, 118);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(57, 20);
+            this.checkBox6.TabIndex = 4;
+            this.checkBox6.Text = "Mısır";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(134, 81);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(66, 20);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Sucuk";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(1, 81);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(63, 20);
+            this.checkBox4.TabIndex = 2;
+            this.checkBox4.Text = "Sosis";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(134, 42);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(68, 20);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Salam";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(1, 42);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Dana Jambon";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(85, 491);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Adet:";
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.Location = new System.Drawing.Point(129, 488);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(71, 22);
+            this.txtAdet.TabIndex = 7;
+            // 
+            // txtSiparisToplam
+            // 
+            this.txtSiparisToplam.Location = new System.Drawing.Point(273, 491);
+            this.txtSiparisToplam.Name = "txtSiparisToplam";
+            this.txtSiparisToplam.Size = new System.Drawing.Size(71, 22);
+            this.txtSiparisToplam.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(210, 491);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Toplam:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(176, 525);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Kapat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(257, 525);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Sipariş Ver";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(-7, -4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(461, 52);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Pizza Sipariş Ekranı";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(447, 596);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSiparisToplam);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAdet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.lstTür);
+            this.Controls.Add(this.cbEbat);
+            this.Controls.Add(this.lblEbat);
+            this.Name = "Form1";
+            this.Text = "Pizza Sipariş";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEbat;
+        private System.Windows.Forms.ComboBox cbEbat;
+        private System.Windows.Forms.ListBox lstTür;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdet;
+        private System.Windows.Forms.TextBox txtSiparisToplam;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Pizza_Siparis_Ekrani-main/PizzaSiparis/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaSiparis
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtSiparisToplam.Enabled = false;
+            double toplamfiyat = 0;
+            int adet = 0;
+          
+            if (int.TryParse(txtAdet.Text, out adet) && adet > 0)
+            {
+                string secilenboyut = cbEbat.SelectedItem.ToString();
+                if (secilenboyut == "Double Small 100Tl")
+                {
+                    toplamfiyat += 100 * adet;
+                }
+                else if (secilenboyut == "Small 120Tl")
+                {
+                    toplamfiyat += 120 * adet;
+                }
+                else if (secilenboyut == "Medium 150TL")
+                {
+                    toplamfiyat += 120 * adet;
+                    
+                }
+                else if (secilenboyut == "Large 210Tl")
+                {
+                    toplamfiyat += 210 * adet;
+                }
+                else if (secilenboyut == "Double Large 250Tl")
+                {
+                    toplamfiyat += 250 * adet;
+                }
+
+                if (radioButton1.Checked)
+                {
+                    toplamfiyat += 15 * adet;
+                }
+            }
+
+            txtSiparisToplam.Text = toplamfiyat.ToString() + "Tl";
+
+
+
+
+        }
+    }
+}
